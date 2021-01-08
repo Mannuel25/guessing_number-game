935c0d1c6a4054e6f6901dbf7cf2eec772ed571c
Tanimowo Emmanuel <68563757+Mannuel25@users.noreply.github.com>
2021-01-08 13:59:02 +0100

    Update GUESSING_NUMBER_GAME.py

--- GUESSING_NUMBER_GAME.py
+++ GUESSING_NUMBER_GAME.py
@@ -2,25 +2,37 @@ import random
 
 def guessingNumberGame():
 	print('Welcome Player!')
+	PICKS = ['YES','Yes','NO','No']
 	play_game = input(f'Do you wish to play?(yes/no): ')
-	number_of_guesses = 0
-	while play_game == 'yes' or play_game == 'YES':
-		print('\nGreat..let\'s get started!')
-		number = int(input('Guess a number between 1 and 100: '))
-		if 1 <= number <= 100:
-			number_of_guesses += 1
-			randomNumber = random.randint(1,100)
-			if number > randomNumber:
-				print('Too high :( ..try again')
-				play_game = input('\nGuess again?(yes/no): ')
-			elif number < randomNumber:
-				print('Too low :( ..try again')
-				play_game = input('\nGuess again?(yes/no): ')
-			else:
-				print('Congratulations {}, you guessed right!!..[Number of guesses: {}]'.format(name,number_of_guesses))
-				play_game = input('\nDo you wish to play again?(yes/no): ')
-		else:
-			print('Out of range..guess a number between 1 and 100')
-			play_game = input('\nGuess again?(yes/no): ')
+	if play_game.title() not in PICKS:
+		print('Invalid input!')
+	else:	
+		number_of_guesses = 0
+		while play_game == 'yes' or play_game == 'YES':
+			try:
+				number = int(input('Guess a number in the range of 1 through 50: '))
+			except ValueError:
+				print('Invalid input!')
+			else:	
+				if 1 <= number <= 50:
+					number_of_guesses += 1
+					randomNumber = random.randint(1,50)
+					if number > randomNumber:
+						print('Too high 😝 ...try again')
+						play_game = input('\nGuess again?(yes/no): ')
+					elif number < randomNumber:
+						print('Too low 😔 ...try again')
+						play_game = input('\nGuess again?(yes/no): ')
+					else:
+						print('Congratulations!! 🎈🎉...you guessed right after {} trials'.format(number_of_guesses))
+						play_game = input('\nDo you wish to play again?(yes/no): ')
+				else:
+					print('Out of range!..guess a number in the range of 1 through 50')
+					play_game = input('\nGuess again?(yes/no): ')
+					if play_game.title() not in PICKS:
+						print('Invalid input!')
+					else:
+						continue	
 
 guessingNumberGame()
+			
